50f73140d0c9e863147b341bfe97a7d03fd2830b
Fubon - Website Design & Frontend Development <43171309+thefubon@users.noreply.github.com>
2022-10-04 20:38:01 +0300

    upd

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-  <div>
-    <NuxtLink class="text-primary-600 hover:text-primary-300" to="/price">Калькулятор стоимости</NuxtLink>
+  <div class="space-y-6">
+    <a class="text-default hover:text-primary-300 font-bold" href="https://github.com/thefubon/nuxt-app" target="_blank">Open GitGub</a>
+    <div>
+      <NuxtLink class="text-primary-600 hover:text-primary-300" to="/price">— Калькулятор стоимости</NuxtLink>
+    </div>
   </div>
 </template>
