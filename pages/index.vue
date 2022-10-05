2b383af0bb5e082fdd808e2cc8ddf96390136429
Fubon - Website Design & Frontend Development <43171309+thefubon@users.noreply.github.com>
2022-10-05 16:06:17 +0300

    upd

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="space-y-6 container">
-    <a class="text-default hover:text-primary-300 font-bold" href="https://github.com/thefubon/nuxt-app" target="_blank">Open GitGub</a>
+    <a class="text-default hover:text-primary-300 font-bold" href="https://github.com/thefubon/nuxt-app" target="_blank">Open GitHub</a>
     <TheNavbar/>
   </div>
 </template>
