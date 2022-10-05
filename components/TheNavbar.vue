b8ced14dc782f0fdd717612cd9672f39e0e1ba82
Fubon - Website Design & Frontend Development <43171309+thefubon@users.noreply.github.com>
2022-10-05 18:04:23 +0300

    upd

--- components/TheNavbar.vue
+++ components/TheNavbar.vue
@@ -5,6 +5,7 @@
     <a class="text-primary-600 hover:text-primary-300 inline-block bg-white text-center rounded p-2" active-class="nav-active" href="/code-input">Input в виде кода</a>
     <a class="text-primary-600 hover:text-primary-300 inline-block bg-white text-center rounded p-2" active-class="nav-active" href="/offcanvas">Off-canvas</a>
     <a class="text-primary-600 hover:text-primary-300 inline-block bg-white text-center rounded p-2" active-class="nav-active" href="/drawer">Drawer</a>
+    <a class="text-primary-600 hover:text-primary-300 inline-block bg-white text-center rounded p-2" active-class="nav-active" href="/modal">Modal</a>
   </div>
 </template>
 
--- pages/drawer.vue
+++ pages/drawer.vue
@@ -1,9 +1,7 @@
 <template>
   <div class="container">
 
-      <button class="toggle" @click="drawerVisible = true">Show Menu</button>
-
-    <h1>Welcome to Vue JS</h1>
+    <button class="toggle" @click="drawerVisible = true">Показать меню</button>
     
     <div
       class="right-drawer overflow-auto"
@@ -16,14 +14,8 @@
         <button class="close" @click="drawerVisible = false">&#9587;</button>
       </div>
       <h1>Menu</h1>
-      <h4>Home</h4>
-      <h4>About</h4>
-      <h4>Stories</h4>
-      <h4>Testimonials</h4>
-      <h4>Contact</h4>
-      <div class="h-[3000px]"></div>
     </div>
-    <!-- We will make the mask fill the screen while the menu is visible. Because its z-index is 1 less than that of the menu, the menu will still be displayed on top of it -->
+    <!-- Мы сделаем так, чтобы маска заполняла весь экран, пока видно меню. Поскольку ее z-индекс на 1 меньше, чем у меню, меню все равно будет отображаться поверх нее -->
     <div
       class="drawer-mask"
       :style="{
@@ -57,27 +49,6 @@ export default {
 </script>
 
 <style scoped>
-#app {
-  font-family: "Segoe UI", Tahoma, Geneva, Verdana, sans-serif;
-}
-
-h1,
-h2,
-h3,
-h4,
-h5 {
-  font-weight: 400;
-  color: #0ca3a3;
-}
-
-h1 {
-  color: teal;
-}
-
-a {
-  color: tomato;
-}
-
 .toggle {
   width: 120px;
   background: white;
