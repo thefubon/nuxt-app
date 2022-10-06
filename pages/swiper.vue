1e34961e8cef74944070a2f1cffb9ff3fc581c4c
Fubon - Website Design & Frontend Development <43171309+thefubon@users.noreply.github.com>
2022-10-06 15:09:32 +0300

    upd

--- pages/swiper.vue
+++ pages/swiper.vue
@@ -16,7 +16,7 @@
         },
       }"
       :modules="modules"
-      class="mySwiper"
+      class="mySwiper overflow-hidden"
     >
       <swiper-slide>Slide 1</swiper-slide>
       <swiper-slide>Slide 2</swiper-slide>
