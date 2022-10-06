b1ec977baee6f6971d453fca9f1bee3c0caeca1e
Fubon - Website Design & Frontend Development <43171309+thefubon@users.noreply.github.com>
2022-10-06 15:04:02 +0300

    upd

--- pages/swiper.vue
+++ pages/swiper.vue
@@ -39,7 +39,6 @@ import "swiper/css";
 import "swiper/css/scrollbar";
 import "swiper/css/navigation";
 import "swiper/css/pagination";
-import "assets/css/plugins/swiper.css";
 
 // import required modules
 import { Scrollbar, Navigation, Pagination } from "swiper";
@@ -58,7 +57,52 @@ export default {
 </script>
 
 <style scoped>
+  .swiper {
+    width: 100%;
+    height: 100%;
+  }
+
   .swiper-slide {
-    @apply h-[400px] bg-indigo-600;
+     @apply h-[400px] bg-indigo-600;
+    text-align: center;
+    font-size: 18px;
+    background: #fff;
+
+    /* Center slide text vertically */
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: -webkit-flex;
+    display: flex;
+    -webkit-box-pack: center;
+    -ms-flex-pack: center;
+    -webkit-justify-content: center;
+    justify-content: center;
+    -webkit-box-align: center;
+    -ms-flex-align: center;
+    -webkit-align-items: center;
+    align-items: center;
+  }
+
+  .swiper-slide img {
+    display: block;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+  }
+
+  .swiper-slide {
+    width: 80%;
+  }
+
+  .swiper-slide:nth-child(2n) {
+    width: 60%;
+  }
+
+  .swiper-slide:nth-child(3n) {
+    width: 40%;
+  }
+
+  .swiper-slide:nth-child(4n) {
+    width: 100%;
   }
 </style>
