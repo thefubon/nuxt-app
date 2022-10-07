449d7f6163d0f29332f43532057865b7e3d5b340
Fubon - Website Design & Frontend Development <43171309+thefubon@users.noreply.github.com>
2022-10-07 15:56:21 +0300

    upd

--- pages/uikit.vue
+++ pages/uikit.vue
@@ -1,6 +1,6 @@
 <template>
 
-  <div class="container space-y-10 h-[3000px]">
+  <div class="container space-y-10">
     
     <div class="text-center">
       <pre class="bg-white py-2 px-4 rounded-lg inline-block">https://getuikit.com/docs/installation</pre>
@@ -9,19 +9,19 @@
     <div uk-margin>
       <ul uk-accordion="multiple: true">
         <li class="uk-open">
-          <a class="uk-accordion-title" href="#">{{ title }}</a>
+          <a class="uk-accordion-title" href="#">{{ title1 }}</a>
           <div class="uk-accordion-content">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
           </div>
         </li>
         <li>
-          <a class="uk-accordion-title" href="#">{{ title }}</a>
+          <a class="uk-accordion-title" href="#">{{ title2 }}</a>
           <div class="uk-accordion-content">
             <p>Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor reprehenderit.</p>
           </div>
         </li>
         <li>
-          <a class="uk-accordion-title" href="#">{{ title }}</a>
+          <a class="uk-accordion-title" href="#">{{ title3 }}</a>
           <div class="uk-accordion-content">
             <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat proident.</p>
           </div>
@@ -30,31 +30,106 @@
     </div>
 
     <div class="uk-background-primary uk-height-large">
-      <div class="uk-card uk-card-default uk-card-body uk-text-center uk-position-z-index" uk-sticky="end: !.uk-height-large; offset: 80">Stick to the top</div>
+      <div class="uk-card uk-card-default uk-card-body uk-text-center uk-position-z-index" data-uk-sticky="end: !.uk-height-large; offset: 80">Stick to the top</div>
     </div>
+      
+    <div class="uk-slider-container-offset" data-uk-slider>
+
+      <div class="uk-position-relative uk-visible-toggle uk-light" tabindex="-1">
+
+        <ul class="uk-slider-items uk-child-width-1-2@s uk-grid">
+          <li>
+            <div class="uk-card uk-card-default">
+              <div class="uk-card-media-top">
+                <img src="https://getuikit.com/docs/images/photo.jpg" width="1800" height="1200" alt="">
+              </div>
+              <div class="uk-card-body">
+                <h3 class="uk-card-title">Headline</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div class="uk-card uk-card-default">
+              <div class="uk-card-media-top">
+                <img src="https://getuikit.com/docs/images/dark.jpg" width="1800" height="1200" alt="">
+              </div>
+              <div class="uk-card-body">
+                <h3 class="uk-card-title">Headline</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div class="uk-card uk-card-default">
+              <div class="uk-card-media-top">
+                <img src="https://getuikit.com/docs/images/light.jpg" width="1800" height="1200" alt="">
+              </div>
+              <div class="uk-card-body">
+                <h3 class="uk-card-title">Headline</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div class="uk-card uk-card-default">
+              <div class="uk-card-media-top">
+                <img src="https://getuikit.com/docs/images/photo2.jpg" width="1800" height="1200" alt="">
+              </div>
+              <div class="uk-card-body">
+                <h3 class="uk-card-title">Headline</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div class="uk-card uk-card-default">
+              <div class="uk-card-media-top">
+                <img src="https://getuikit.com/docs/images/photo3.jpg" width="1800" height="1200" alt="">
+              </div>
+              <div class="uk-card-body">
+                <h3 class="uk-card-title">Headline</h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt.</p>
+              </div>
+            </div>
+          </li>
+        </ul>
+
+        <a class="uk-position-center-left uk-position-small uk-hidden-hover" href="#" data-uk-slidenav-previous data-uk-slider-item="previous"></a>
+        <a class="uk-position-center-right uk-position-small uk-hidden-hover" href="#" data-uk-slidenav-next data-uk-slider-item="next"></a>
+
+      </div>
+
+      <ul class="uk-slider-nav uk-dotnav uk-flex-center uk-margin"></ul>
+
+    </div>
+
+    <video src="https://yootheme.com/site/images/media/yootheme-pro.mp4" width="1800" height="1200" loop muted playsinline data-uk-video="autoplay: inview"></video>
 
   </div>
 
 </template>
 
 <script setup>
-const title = ref('Hello World')
+  const title1 = ref('Item 1')
+  const title2 = ref('Item 2')
+  const title3 = ref('Item 3')
 
-useHead({
-  titleTemplate: (titleChunk) => {
-    return titleChunk ? `${titleChunk} - UiKit Framework` : 'UiKit Framework';
-  },
-  link: [
-    { 
-      rel: 'stylesheet', 
-      href: '/css/uikit.css'
+  useHead({
+    titleTemplate: (titleChunk) => {
+      return titleChunk ? `${titleChunk} - UiKit Framework` : 'UiKit Framework';
     },
-  ],
-  script: [
-    {
-      src: '/js/uikit.min.js',
-      body: false
-    }
-  ]
-})
+    link: [
+      { 
+        rel: 'stylesheet', 
+        href: '/css/uikit.css'
+      },
+    ],
+    script: [
+      {
+        src: '/js/uikit.min.js',
+        body: false
+      }
+    ]
+  })
 </script>
