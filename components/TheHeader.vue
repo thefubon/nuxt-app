bb363adfa0cae34b55454ea900384c211a9a0831
Fubon - Website Design & Frontend Development <43171309+thefubon@users.noreply.github.com>
2022-10-09 23:52:28 +0300

    upd

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -2,7 +2,7 @@
   <header class="bg-white py-4">
     <nav class="container px-4 md:flex justify-between items-center space-y-4 md:space-y-0">
       <h1 class="text-2xl font-bold"><a href="/">Nuxt<sup>3</sup> | Components Collection</a></h1>
-      <a class="button button-primary button-sm" href="https://github.com/thefubon/nuxt-app" target="_blank"><IconGitHub class="inline-block mr-2"/>GitHub</a>
+      <a class="button button-primary button-sm w-full md:w-auto" href="https://github.com/thefubon/nuxt-app" target="_blank"><IconGitHub class="inline-block mr-2"/>GitHub</a>
     </nav>
   </header>
 </template>
