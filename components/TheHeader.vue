c0c58cc2629b6dc87c97ce9c2d80feea61c1aefb
Fubon - Website Design & Frontend Development <43171309+thefubon@users.noreply.github.com>
2022-10-11 10:47:36 +0300

    upd

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,8 +1,8 @@
 <template>
   <header class="bg-white py-4">
-    <nav class="container px-4 md:flex justify-between items-center space-y-4 md:space-y-0">
-      <h1 class="text-2xl font-bold"><a href="/">Nuxt<sup>3</sup> | Components Collection</a></h1>
-      <a class="button button-primary button-sm w-full md:w-auto" href="https://github.com/thefubon/nuxt-app" target="_blank"><IconGitHub class="inline-block mr-2"/>GitHub</a>
+    <nav class="container px-4 flex justify-between items-center">
+      <h1 class="text-2xl font-bold"><a class="flex flex-col md:flex-row md:items-center md:space-x-2 leading-tight md:leading-none" href="/"><span>Nuxt<sup>3</sup></span> <span class="hidden md:inline-block">|</span> <span class="text-sm font-normal md:text-2xl">Components Collection</span></a></h1>
+      <a class="button button-primary button-sm !px-4 md:!px-6" href="https://github.com/thefubon/nuxt-app" target="_blank"><IconGitHub class="inline-block mr-2"/>GitHub</a>
     </nav>
   </header>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <TheHeader class="sticky top-0 z-10"/>
-    <main class="container px-4 py-10">
+    <TheHeader/>
+    <main class="container px-4 py-6 md:py-10">
       <slot />
     </main>
     <TheFooter/>
