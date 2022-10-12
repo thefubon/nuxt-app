147b4773d9aca79ca0821e9925bcd8d542e62572
Fubon - Website Design & Frontend Development <43171309+thefubon@users.noreply.github.com>
2022-10-12 14:50:09 +0300

    upd

--- pages/index.vue
+++ pages/index.vue
@@ -5,8 +5,15 @@
 
     <TheNavbar/>
 
-    <div class="space-x-4 py-4 text-sm">
-      &copy; 2022 <NuxtLink to="http://thefubon.com" target="_blank">Fubon</NuxtLink> - Website Design & Frontend Development
+    <div class="space-x-4 py-4 text-sm flex items-center">
+      <select class="dark:text-black focus:ring-0 focus:outline-none border-none rounded" v-model="$colorMode.preference">
+        <option value="system">System</option>
+        <option value="light">Light</option>
+        <option value="dark">Dark</option>
+      </select>
+     <div>
+       &copy; 2022 <NuxtLink to="http://thefubon.com" target="_blank">Fubon</NuxtLink> - Website Design & Frontend Development
+     </div>
     </div>
 
   </div>
