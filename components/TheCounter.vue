98e5f87c1b9065b57ac732030fd7342fae75921d
Fubon - Website Design & Frontend Development <43171309+thefubon@users.noreply.github.com>
2022-10-16 19:12:12 +0300

    upd

--- components/TheCounter.vue
+++ components/TheCounter.vue
@@ -1,10 +1,6 @@
 <template>
   <div>
-    <!--LiveInternet counter--><a href="https://www.liveinternet.ru/click"
-    target="_blank"><img id="licntC4F4" width="88" height="31" style="border:0" 
-    title="LiveInternet: показано число просмотров за 24 часа, посетителей за 24 часа и за сегодня"
-    src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAEALAAAAAABAAEAAAIBTAA7"
-    alt=""/></a><!--/LiveInternet-->
+    <img id="licnt698F" width="88" height="31" style="border:0" title="LiveInternet: показано число просмотров за 24 часа, посетителей за 24 часа и за сегодня" src="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///yH5BAEAAAEALAAAAAABAAEAAAIBTAA7" alt=""/>
   </div>
 </template>
 
--- public/js/counter.js
+++ public/js/counter.js
@@ -1,4 +1,4 @@
-(function(d,s){d.getElementById("licntC4F4").src=
+(function(d,s){d.getElementById("licnt698F").src=
 "https://counter.yadro.ru/hit?t14.1;r"+escape(d.referrer)+
 ((typeof(s)=="undefined")?"":";s"+s.width+"*"+s.height+"*"+
 (s.colorDepth?s.colorDepth:s.pixelDepth))+";u"+escape(d.URL)+
